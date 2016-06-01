fed0a9cc7424fb82e4ac27b8f4b98e87df770be9
QuentinGeorge <quentingeorge7@gmail.com>
2016-06-01 23:53:48 +0200

    add audio player bar

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,85 @@
-( function() {
+"use strict";
 
-    "use strict";
+module.exports = function( grunt ) {
 
-    module.exports = function( grunt ) {
+    // 1. load tasks
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-cowsay" );
+    grunt.loadNpmTasks( "grunt-eslint" );
+    grunt.loadNpmTasks( "grunt-browser-sync" );
+    grunt.loadNpmTasks( "grunt-sass" );
 
-        // 1. load tasks
-        grunt.loadNpmTasks( "grunt-contrib-watch" );
-        grunt.loadNpmTasks( "grunt-cowsay" );
-        grunt.loadNpmTasks( "grunt-eslint" );
-
-        // 2. configure tasks
-        grunt.initConfig( {
-            // cowsay
-            "cowsay": {
-                "done": {
-                    "options": {
-                        "message": "I'm done!"
-                    }
-                }
-            },
-            // eslint
-            "eslint": {
-                "options": {
-                    "configFile": ".eslintrc.json"
+    // 2. configure tasks
+    grunt.initConfig( {
+        // browser sync
+        "browserSync": {
+            "preview": {
+                "bsFiles": {
+                    "src": "css/*.css"
                 },
-                "scripts": [ "*.js" ]
-            },
-            // watch
-            "watch": {
                 "options": {
-                    "spawn": false
-                },
-                "scripts": {
-                    "files": [ "*.js" ],
-                    "tasks": [ "eslint" ]
+                    "proxy": "localhost/jeu-video/game-design/", // TODO: adapt to you configuration
+                    "watchTask": true
                 }
             }
-        } );
+        },
+        // cowsay
+        "cowsay": {
+            "done": {
+                "options": {
+                    "message": "I'm done!"
+                }
+            }
+        },
+        // eslint
+        "eslint": {
+            "options": {
+                "configFile": ".eslintrc.json"
+            },
+            "scripts": [ "scripts/**/*.js" ]
+        },
+        // sass
+        "sass": {
+            "options": {
+                "sourceMap": true
+            },
+            "styles": {
+                "files": {
+                    "css/styles.css": "sass/styles.scss"
+                }
+            }
+        },
+        // watch
+        "watch": {
+            "options": {
+                "spawn": false
+            },
+            "scripts": {
+                "files": [ "scripts/**/*.js" ],
+                "tasks": [ "eslint" ]
+            },
+            "styles": {
+                "files": [ "sass/**/*.scss" ],
+                "tasks": [ "sass:styles" ]
+            }
+        }
+    } );
 
-        // 3. aliases
-        grunt.registerTask( "default", [
-            "analyse",
-            "cowsay:done"
-        ] );
+    // 3. aliases
+    grunt.registerTask( "default", [
+        "build",
+        "analyse",
+        "cowsay:done"
+    ] );
 
-        grunt.registerTask( "analyse", [ "eslint:scripts" ] );
+    grunt.registerTask( "build", [ "sass:styles" ] );
 
-        grunt.registerTask( "work", [
-            "analyse",
-            "watch"
-        ] );
-    };
+    grunt.registerTask( "analyse", [ "eslint:scripts" ] );
 
-} )();
+    grunt.registerTask( "work", [
+        "build",
+        "analyse",
+        "browserSync",
+        "watch"
+    ] );
+};
---
+++ scripts/audio.js
@@ -0,0 +1,69 @@
+( function() {
+    "use strict";
+
+    var mAudioPlayer = document.getElementById( "player" ),
+        mPlayPause = document.getElementById( "control-play-pause" ),
+        mMute = document.getElementById( "control-mute" ),
+        mVolume = document.getElementById( "control-volume" ),
+        fModifiVolumeIcon;
+
+    fModifiVolumeIcon = function( mAudioPlayer, mMute ) {
+        if ( mAudioPlayer.volume === 0 ) {
+            mMute.style.backgroundImage = "url('./resources/img/soundsSvg/volume-mute.svg')";
+            return;
+        }
+        if ( mAudioPlayer.volume > 0 && mAudioPlayer.volume <= 0.33 ) {
+            mMute.style.backgroundImage = "url('./resources/img/soundsSvg/volume-low.svg')";
+            return;
+        }
+        if ( mAudioPlayer.volume > 0.33 && mAudioPlayer.volume <= 0.66 ) {
+            mMute.style.backgroundImage = "url('./resources/img/soundsSvg/volume-medium.svg')";
+            return;
+        }
+        if ( mAudioPlayer.volume > 0.66 ) {
+            mMute.style.backgroundImage = "url('./resources/img/soundsSvg/volume-high.svg')";
+            return;
+        }
+    };
+
+    // set images faster when page is loaded and music player not yet
+    mPlayPause.style.backgroundImage = "url('./resources/img/soundsSvg/play.svg')";
+    mMute.style.backgroundImage = "url('./resources/img/soundsSvg/volume-medium.svg')";
+
+    mAudioPlayer.oncanplay = function() {
+        mAudioPlayer.volume = mVolume.value;
+        fModifiVolumeIcon( mAudioPlayer, mMute );
+
+        mPlayPause.onclick = function() {
+            mPlayPause.blur();
+            if ( mAudioPlayer.paused === true ) {
+                mAudioPlayer.play();
+                mPlayPause.style.backgroundImage = "url('./resources/img/soundsSvg/pause.svg')";
+            } else {
+                mAudioPlayer.pause();
+                mPlayPause.style.backgroundImage = "url('./resources/img/soundsSvg/play.svg')";
+            }
+        };
+
+        mMute.onclick = function() {
+            mMute.blur();
+            if ( mAudioPlayer.muted === true ) {
+                mAudioPlayer.muted = false;
+            } else {
+                mAudioPlayer.muted = true;
+            }
+        };
+
+        mVolume.onclick = function() {
+            mVolume.blur();
+            fModifiVolumeIcon( mAudioPlayer, mMute );
+        };
+
+        mVolume.oninput = function() {
+            mVolume.blur();
+            mAudioPlayer.volume = mVolume.value;
+            fModifiVolumeIcon( mAudioPlayer, mMute );
+        };
+    };
+
+} )();
