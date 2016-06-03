c8e06db76e1ffc458d93c9f51cdf2e1f6df72708
QuentinGeorge <quentingeorge7@gmail.com>
2016-06-03 23:53:54 +0200

    center game on the screen

--- scripts/game.js
+++ scripts/game.js
@@ -28,4 +28,40 @@
     };
 
     oApp.setup();
+
+    // Center game on click to help on mobile version if screen < body
+    document.querySelector( ".container" ).onclick = function() {
+        // Center the game verticaly on body if window >= 650px, if 650 > window > 550 on Canvas + audio controls, and on Canvas if window <= 550px
+        var oPositions;
+
+        if ( window.innerHeight >= 650 ) {
+            oPositions = document.body.getBoundingClientRect();
+        } else {
+            oPositions = document.querySelector( ".container" ).getBoundingClientRect();
+            if ( window.innerHeight <= 550 ) {
+                oPositions = document.querySelector( "#game" ).getBoundingClientRect();
+            }
+        }
+        window.scrollTo( 0, ( oPositions.top + window.pageYOffset - ( window.innerHeight / 2 ) + ( oPositions.height / 2 ) ) );
+    };
+
+    // Center the game with space-bar
+    window.onkeydown = function( oEvent ) {
+        // Center the game verticaly on body if window >= 650px, if 650 > window > 550 on Canvas + audio controls, and on Canvas if window <= 550px
+        var oPositions;
+
+        if ( window.innerHeight >= 650 ) {
+            oPositions = document.body.getBoundingClientRect();
+        } else {
+            oPositions = document.querySelector( ".container" ).getBoundingClientRect();
+            if ( window.innerHeight <= 550 ) {
+                oPositions = document.querySelector( "#game" ).getBoundingClientRect();
+            }
+        }
+        if ( oEvent.keyCode === 32 && oEvent.target === document.body ) {
+            oEvent.preventDefault(); // Avoid the space-bar scroll down matter
+            window.scrollTo( 0, ( oPositions.top + window.pageYOffset - ( window.innerHeight / 2 ) + ( oPositions.height / 2 ) ) );
+        }
+    };
+
 } )( window.Shinobi );
